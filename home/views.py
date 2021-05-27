65a7246b318128469ef92449fbdcf341cc6d5a11
Shubham <starinfosol954@gmail.com>
2021-05-27 18:26:36 +0530

    login

--- home/views.py
+++ home/views.py
@@ -31,8 +31,7 @@ def contact(request):
         if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
             messages.error(request, "Please fill the form correctly")
         else:
-            contact = Contact(name=name, email=email,
-                              phone=phone, content=content)
+            contact = Contact(name=name, email=email,phone=phone, content=content)
             contact.save()
             messages.success(
                 request, "Your message has been successfully sent")
